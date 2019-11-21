86ba2dd2163e6402539baef45a3c0bd947da32e8
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2019-11-21 17:39:18 +0000

    sorting out the main index which loads everything so thats now global and calls some shit for specific projects

---
+++ backend/ProfilesRouter.js
@@ -0,0 +1,47 @@
+module.exports = new (function(){
+	const MESSAGE='message';
+	const EventEnabledBuilder = require('core').EventEnabledBuilder;
+	const ItemTypes = require('enums')ItemTypes;
+	const ItemRouter = require('./ItemRouter');
+	var _profilesRouter;
+	this.get=function(){
+		if(!_profilesRouter){
+			_profilesRouter = new ProfilesRouter();
+		}
+		return _profilesRouter;
+	};
+	function ProfilesRouter(){
+		EventEnabledBuilder(this);
+		var self = this;
+		var profilesCache;
+		var itemRouter = new ItemRouter({itemType:ItemTypes.PROFILE, getIds:getIds});
+		this.getChannels=itemRouter.getChannels;
+		this.getRoutingTable=itemRouter.getRoutingTable;
+		this.getMyIp = itemRouter.getMyIp;
+		this.sendToServersWith=itemRouter.sendToServersWithItem;
+		this.getSendToServerWithTryHandle = itemRouter.getSendToServerWithItemTryHandle
+		this.has = itemRouter.hasItem;
+		this.add=itemRouter.addItem;
+		this.remove = itemRouter.removeItem;
+		this.setProfilesCache = function(value){
+			profilesCache=value;
+		};
+		itemRouter.incoming = incoming;
+		function getIds(){
+			return profilesCache.getLocalIds();
+		}
+		function incoming(msg, channel, itemId){
+			validateHasProfileAndSendRoutingTableIfNecessary(itemId);
+			dispatchOnMessage(msg, channel);
+		}
+		function validateHasProfileAndSendRoutingTableIfNecessary(itemId, channel){
+			if(!itemId)return;
+			if(profilesCache.hasLocal(itemId))return;
+			itemRouter.sendRoutingTable(channel.getIp());
+		}
+		function dispatchOnMessage(msg, channel){
+			var msg = {type:MESSAGE, msg:msg, channel:channel.getPublic()};
+			self.dispatchEvent(msg);
+		}
+	}
+})();
