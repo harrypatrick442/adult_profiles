cff0a325ff4b124f2ff3717ea3e44dd3987e6501
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2019-12-06 02:37:06 +0000

    fixed issue with router initialization

--- backend/ProfileHelper.js
+++ backend/ProfileHelper.js
@@ -53,6 +53,7 @@ module.exports = new (function(){
 	function sendProfileUpdatesToAllUserDevices(userId, profile){
 		var msg = {type:S.CLIENT_PROFILE_UPDATES, userId:userId, profile:profile};	
 		var user = users.getById(userId);
+		console.log('by id user');
 		if(user)user.getDevices().sendMessage(msg);
 		UsersRouter.get().sendToServersWith(userId, msg);
 	}
