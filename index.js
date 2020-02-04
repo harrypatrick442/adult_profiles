9ab4363b648dcc5f5cee5d6da69b53befc84e28d
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2020-02-04 15:11:53 +0000

    fixed

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const RepositoriesHelper = require('repositories_helper');module.exports={
+	get DalProfiles(){return require('./backend/DalProfiles')},
+	get DalSearchMembersCriteria(){return require('./backend/DalSearchMembersCriteria')},
+	get MembersRepository(){return require('./backend/MembersRepository')},
+	get ProfileHandler(){return require('./backend/ProfileHandler')},
+	get ProfileHelper(){return require('./backend/ProfileHelper')},
+	get ProfileRepository(){return require('./backend/ProfileRepository')},
+	get ProfilesRouter(){return require('./backend/ProfilesRouter')},
+	get SearchMembersCache(){return require('./backend/SearchMembersCache')},
+
 	getScriptsRelativePath:RepositoriesHelper.getGetScriptsRelativePath(),
 	getScriptsAbsolutePath:RepositoriesHelper.getGetScriptsAbsolutePath(),
 	getStylesRelativePath:RepositoriesHelper.getGetStylesRelativePath(),
